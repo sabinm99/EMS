fd1a67ef37aeff38d19630a5314d09273c0868b7
Sabin <sabinxmihalcea@gmail.com>
2023-09-14 14:09:27 +0300

    Create add company window

---
+++ src/main/java/Controller.java
@@ -0,0 +1,12 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class Controller {
+    @FXML
+
+    public void addCompany (ActionEvent e){
+        System.out.println("add company pressed");
+
+    }
+}
