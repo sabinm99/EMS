bf901613afdbd54581a984222c37ef855c19a0fa
Sabin <sabinxmihalcea@gmail.com>
2023-09-14 14:13:31 +0300

    Create add company window and Controller class

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,12 +1,27 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
 
-    public void addCompany (ActionEvent e){
-        System.out.println("add company pressed");
-
+    public void addCompany(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("addCompanyWindow.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
+
